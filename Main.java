7d59d4d8a93bd3a0902abbfadf2e90d28c60be01
Maximeowie <maxeenjayde.26@gmail.com>
2025-05-09 15:38:32 +0000

    taskk 2 done

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b = 3;
+        short s = 11;
+        int i = 0;
+        float f = 2.0f;
+        char c1 = 'H';
+        char c2 = 'w';
+        char c3 = 'O';
+        char c4 = 'r';
+        char c5 = 'l';
+        char c6 = 'd';
+        boolean bool = true;
+
+       
+        String result = "" + c1 + b + s + i + " " + c2 + c3 + c4 + c5 + c6 + " " + f + " " + bool;
+
+        
+        System.out.println(result);
+
+        
+        byte zero = 0;
+        String output = "W" + zero + "w";
+        
+    }
+}
